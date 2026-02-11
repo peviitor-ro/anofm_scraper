2971785f00088e191549ff42a0dee85bcffdcd9c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-02-11 03:07:14 +0200

    Update edujobs_runner workflow to use SERVER_URL environment variable and add bestjobs_runner workflow

--- edujobs.py
+++ edujobs.py
@@ -1,54 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
-from utils import get_token, GetCounty, main, remove_diacritics
+from utils import get_token, GetCounty, main, remove_diacritics, remove_company
 from concurrent.futures import ThreadPoolExecutor
+from math import ceil
 
 _counties = GetCounty()
 
-url = "https://edujobs.ro/jm-ajax/get_listings/"
+url = "https://back-edujobs.feel-it-services.com/job-postings/query?favoritesFirst=false"
 
-payload = f"search_keywords=&search_location=&filter_job_type%5B%5D=full-time&filter_job_type%5B%5D=internship&filter_job_type%5B%5D=part-time&filter_job_type%5B%5D=sezonier&filter_job_type%5B%5D=&per_page=15000&orderby=featured&featured_first=true&order=DESC&page=1&remote_position=&show_pagination=false&form_data=search_keywords%3D%26search_location%3D%26search_salary_min%3D%26search_salary_max%3D%26search_rate_min%3D%26search_rate_max%3D%26filter_job_type%255B%255D%3Dfull-time%26filter_job_type%255B%255D%3Dinternship%26filter_job_type%255B%255D%3Dpart-time%26filter_job_type%255B%255D%3Dsezonier%26filter_job_type%255B%255D%3D&job_layout=_list&job_version=3"
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Cookie': 'pmpro_visit=1'
-}
+payload = {"page": 1, "limit": 99}
 
-response = requests.request("POST", url, headers=headers, data=payload).json()
 
-html = response.get("html", "")
+response = requests.request("POST", url, data=payload).json()
 
-soup = BeautifulSoup(html, 'html.parser')
-
-job_cards = soup.find_all('li', class_='job-list')
+pages = ceil(response.get("totalScrapedJobs", 0)
+             / payload["limit"])
 
 company_jobs = {}
+while payload["page"] <= pages:
+    for job in response.get("scrapedJobs", []):
+        job_title = job.get("title")
+        job_link = f"https://edujobs.ro/job-page/{job.get('scrapedJobId')}"
+        company_name = "Edujobs"
+        try:
+            city = remove_diacritics(job.get("location").split(",")[0].strip())
+            county = _counties.get_county(city)
+        except AttributeError:
+            city = []
+            county = []
+        if job_title:
+            job_data = {
+                "job_title": job_title,
+                "job_link": job_link,
+                "country": "Romania",
+                "city": city,
+                "county": county,
+                "company": company_name,
+                "source": "EDUJOBS"
+            }
 
-for card in job_cards:
-    job_title = card.find('h2', class_='title').text.strip()
-    job_link = card.find('div', class_='job-title').find('a')['href']
-    company_name = card.find('span', class_='company').text.replace("Companie:", "").strip()
-    city = remove_diacritics(
-        card.find('span', class_='location').text.replace("Locație:", "").strip())
-    
-    county = _counties.get_county(city)
+            if company_name not in company_jobs:
+                company_jobs[company_name] = {
+                    "logo": None,
+                    "name": company_name,
+                    "jobs": []
+                }
+            company_jobs[company_name]["jobs"].append(job_data)
 
-    job_data = {
-        "job_title": job_title,
-        "job_link": job_link,
-        "country": "Romania",
-        "city": city,
-        "county": county,
-        "company": company_name,
-        "source": "EDUJOBS"
-    }
-    
-    if company_name not in company_jobs:
-        company_jobs[company_name] = {
-            "logo": card.find('img', class_='company_logo')['src'] if card.find('img', class_='company_logo') else None,
-            "name": company_name,
-            "jobs": []
-        }
-    company_jobs[company_name]["jobs"].append(job_data)
+    print(f"Page {payload['page']} of {pages} scraped.")
+    payload["page"] += 1
+    response = requests.request("POST", url, data=payload).json()
 
 TOKEN = get_token()
 
@@ -62,7 +63,9 @@ def start(jobs):
             json=[{"id": jobs.get("name"), "logo": jobs.get("logo")}],
         )
 
+
 MAX_WORKERS = 5
+remove_company("Edujobs", TOKEN)
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
     for company_id, jobs in company_jobs.items():
         executor.submit(start, jobs)
